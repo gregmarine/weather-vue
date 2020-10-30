c31fe3f74fbda9d30f56349eea8cf6e56ff4e415
Greg Marine <gregmarine@iccnet.org>
2020-10-30 15:42:36 -0400

    More sanity testing

--- api/GetCurrent/index.ts
+++ api/GetCurrent/index.ts
@@ -8,9 +8,9 @@ const GetCurrent: AzureFunction = async function (context: Context, req: HttpReq
         body: `https://api.weatherapi.com/v1/current.json?key=${process.env['WEATHER_API_KEY']}&q=${location}`
     }
 
-    /*
+    
     const res = await axios.get(`https://api.weatherapi.com/v1/current.json?key=${process.env['WEATHER_API_KEY']}&q=${location}`);
-    context.res = {
+    /*context.res = {
         status: res.status,
         body: res.data
     }
