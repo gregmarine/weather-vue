c2f1cc34c6cc0af86e5c31b2be748196e33cad43
Greg Marine <gregmarine@iccnet.org>
2020-10-30 16:16:54 -0400

    Changed to use promise over await

--- api/GetCurrent/index.ts
+++ api/GetCurrent/index.ts
@@ -4,11 +4,19 @@ import axios from 'axios';
 const GetCurrent: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     const location = (req.query.location);
 
-    const res = await axios.get('https://www.gregmarine.com'); //`https://api.weatherapi.com/v1/current.json?key=${process.env['WEATHER_API_KEY']}&q=${location}`);
-    context.res = {
-        status: res.status,
-        body: res.data
-    }
+    axios.get(`https://api.weatherapi.com/v1/current.json?key=${process.env['WEATHER_API_KEY']}&q=${location}`)
+        .then(res => {
+            context.res = {
+                status: res.status,
+                body: res.data
+            }
+        })
+        .catch(err => {
+            context.res = {
+                status: 200,
+                body: err.data
+            }
+        });
 };
 
 export default GetCurrent;
