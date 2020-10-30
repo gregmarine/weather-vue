2c77907bd0957df65f4b325427212c1cdd3e037e
Greg Marine <gregmarine@iccnet.org>
2020-10-30 15:53:31 -0400

    Testing getting URL

--- api/GetCurrent/index.ts
+++ api/GetCurrent/index.ts
@@ -4,17 +4,11 @@ import axios from 'axios';
 const GetCurrent: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     const location = (req.query.location);
 
+    const res = await axios.get('https://www.gregmarine.com'); //`https://api.weatherapi.com/v1/current.json?key=${process.env['WEATHER_API_KEY']}&q=${location}`);
     context.res = {
-        body: `https://api.weatherapi.com/v1/current.json?key=${process.env['WEATHER_API_KEY']}&q=${location}`
-    }
-
-    
-    const res = await axios.get(`https://api.weatherapi.com/v1/current.json?key=${process.env['WEATHER_API_KEY']}&q=${location}`);
-    /*context.res = {
         status: res.status,
         body: res.data
     }
-    */
 };
 
 export default GetCurrent;
