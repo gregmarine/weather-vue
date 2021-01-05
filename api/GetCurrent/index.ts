769cacabe620e167915a3c3372131a9286719654
Greg Marine <gregmarine@iccnet.org>
2021-01-05 14:02:35 -0500

    Updated to use node-fetch

--- api/GetCurrent/index.ts
+++ api/GetCurrent/index.ts
@@ -1,22 +1,15 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import axios from 'axios';
+import fetch from 'node-fetch';
 
 const GetCurrent: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     const location = (req.query.location);
+    
+    const res = await fetch(`https://api.weatherapi.com/v1/current.json?key=${process.env['WEATHER_API_KEY']}&q=${location}`)
+    const weather = await res.json();
 
-    axios.get(`https://api.weatherapi.com/v1/current.json?key=${process.env['WEATHER_API_KEY']}&q=${location}`)
-        .then(res => {
-            context.res = {
-                status: res.status,
-                body: res.data
-            }
-        })
-        .catch(err => {
-            context.res = {
-                status: 200,
-                body: err.data
-            }
-        });
+    context.res = {
+        body: weather
+    };
 };
 
 export default GetCurrent;
